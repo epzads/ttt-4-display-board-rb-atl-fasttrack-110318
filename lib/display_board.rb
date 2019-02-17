c8714a254d3ae1ae4728f59c84cc239c0e51da87
favorable-vector-1743 <epzads@gmail.com>
2019-02-17 16:39:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,23 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board ( board = [" "," "," "," "," "," "," "," "," "] )
+
+  #puts "A Tic Tac Toe Board"
+
+# puts "```"
+puts board[0]
+ puts "#{board[0]} | #{board[1]} | #{board[2]}"
+ puts "-----------"
+ puts "#{board[3]} | #{board[4]} | #{board[5]}"
+
+# puts "   |   |   "
+ puts "-----------"
+ puts "#{board[6]} | #{board[7]} | #{board[8]}"
+
+
+# puts "   |   |   "
+
+ #puts "```"
+end
+
+display_board ([" x ", " o ", " x ", " x ", " x ", " o ", " o ", " o ", " o "])
